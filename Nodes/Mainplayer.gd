0c90aed92bce020827844669dc91c3df72008826
zeldris <googling3456@gmail.com>
2025-05-26 00:51:23 +0530

    Removed MainPlayer.gd

--- Nodes/Mainplayer.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-#signal emmitted to mainGame
-signal shoot(pos,direc)
-
-func _process(delta: float) -> void:
-	#shoot
-	if Input.is_action_pressed("shoot"):
-		shoot.emit(self.global_position,direction)
