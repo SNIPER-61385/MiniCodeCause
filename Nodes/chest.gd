e58c33e454bbd7ee9020a6e3451ed76af09df1bd
priyanshudebnath <38852086+priyanshudebnath@users.noreply.github.com>
2025-05-25 17:01:38 +0530

    Spawn powerup from object

--- Nodes/chest.gd
+++ Nodes/chest.gd
@@ -1,4 +1,12 @@
 extends container
 
+@onready var chestHitOnce = false
+
 func hit():
-	print("chestbody")
+	if not chestHitOnce:
+		$Sprite2D/AnimationPlayer.play("chestOpen")
+		chestHitOnce = true
+		await $Sprite2D/AnimationPlayer.animation_finished
+		open.emit(global_position,current_direction)
+		
+	
